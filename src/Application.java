0755a36595ec7e742fb4b6369ca6aee915343bdb
Mihail Petrov <mail@mihail-petrov.me>
2022-04-12 20:31:45 +0300

    global - create separate methods for every single process

--- src/Application.java
+++ src/Application.java
@@ -1,27 +1,69 @@
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) {
 
-        Scanner inputScanner = new Scanner(System.in);
+    public static Scanner inputScanner  = new Scanner(System.in);
+    public static boolean isRunning     = true;
+
+    public static void processMove() {
+
         final int OBJECT_WALL   = 1;
         final int OBJECT_CHAIR  = 2;
         final int NO_OBJECT     = 3;
+        final int PROCESS_EXIT  = 4;
+        boolean isRunning       = true;
 
-        while(true) {
+        while(isRunning) {
+
             System.out.println("Какво има на пътя ми ? ");
             System.out.println("1. Стена ");
             System.out.println("2. Стол ");
             System.out.println("3. Чисто е ");
+            System.out.println("4. Изход от процедурата ");
 
             int objectId = inputScanner.nextInt();
 
             switch (objectId) {
-                case 1 -> System.out.println("Go Sideway");
-                case 2 -> System.out.println("Jump");
-                case 3 -> System.out.println("Go Forward");
-                default -> System.out.println("*** Repeat ***");
+
+                case OBJECT_WALL    -> System.out.println("Go Sideway");
+                case OBJECT_CHAIR   -> System.out.println("Jump");
+                case NO_OBJECT      -> System.out.println("Go Forward");
+                case PROCESS_EXIT   -> isRunning = false;
+                default             -> System.out.println("*** Repeat ***");
             }
         }
+
+        System.out.println("Робота спря да се движи");
+    }
+
+    public static void processFight() {
+
+    }
+
+    public static void processExit() {
+        isRunning = false;
+    }
+
+    public static void main(String[] args) {
+
+        final int PROCESS_MOVE  = 1;
+        final int PROCESS_FIGHT = 2;
+        final int PROCESS_EXIT  = 3;
+
+        while (isRunning) {
+            System.out.println("1. Движение");
+            System.out.println("2. Бой");
+            System.out.println("3. Изключване на робота");
+
+            int processCode = inputScanner.nextInt();
+
+            switch (processCode) {
+                case PROCESS_MOVE   -> processMove();
+                case PROCESS_FIGHT  -> processFight();
+                case PROCESS_EXIT   -> processExit();
+            }
+        }
+
+        System.out.println("Робота е изключен");
     }
 }
