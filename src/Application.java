5ca6f6a21461b4bb36f646ab14a0b6637d64b1d6
Mihail Petrov <mail@mihail-petrov.me>
2022-04-12 19:55:17 +0300

    task 1 - create basic navigation system

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
 
+        Scanner inputScanner = new Scanner(System.in);
+        final int OBJECT_WALL   = 1;
+        final int OBJECT_CHAIR  = 2;
+        final int NO_OBJECT     = 3;
+
+        while(true) {
+            System.out.println("Какво има на пътя ми ? ");
+            System.out.println("1. Стена ");
+            System.out.println("2. Стол ");
+            System.out.println("3. Чисто е ");
+
+            int objectId = inputScanner.nextInt();
+            boolean isWallDetected  = (objectId == OBJECT_WALL);
+            boolean isChairDetected = (objectId == OBJECT_CHAIR);
+            boolean isClear         = (objectId == NO_OBJECT);
+
+            if(isWallDetected) {
+                System.out.println("Go Sideway");
+            }
+
+            if(isChairDetected) {
+                System.out.println("Jump");
+            }
+
+            if(isClear) {
+                System.out.println("Go Forward");
+            }
+        }
     }
 }
